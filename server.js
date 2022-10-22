235b9cdb97e62261deea7ca652a14d40f3ae7782
Ramsey-Gicharu <richardgicharu1999@gmail.com>
2022-10-22 12:42:00 +0300

    Admin and User page made

--- server.js
+++ server.js
@@ -1,54 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const app = express();
-const jsonParser = bodyParser.json();
-const fileName = 'students.json';
-
-// // Load data from file
-let rawData = fs.readFileSync(fileName);
-let data = JSON.parse(rawData);
-
+var express = require('express');
+var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+//setting up the css file for the app
+app.use(express.static(__dirname + '/public'));
 
+// use res.render to load up an ejs view file
+
+// adminPage
 app.get('/', function(req, res) {
-  res.render('pages/homeAdmin');
+  res.render('pages/adminPage');
 });
 
-// // link to add students page for admin on navbar
-app.get('/addstudent', function(req, res) {
-  res.render('pages/studentAdminLink');
+// userPage
+app.get('/User', function(req, res) {
+  res.render("pages/userView", {
+        user: "John Doe",
+    });
 });
-
-// // link to add lecturers page for admin on navbar
-app.get('/addlecturer', function(req, res) {
-  res.render('pages/lecturersAdminLink');
-});
-
-// // link to add courses page for admin on navbar
-app.get('/addcourse', function(req, res) {
-  res.render('pages/coursesAdminLink');
-});
-
-// tried making the code write to json file failed
-
-// This is a RESTful GET web service
-app.get('/students', (request, response) => {
-    data.sort((a, b) => (a.name > b.name) ? 1 : -1 );
-    response.send(data);
-});
-
-// This is a RESTful POST web service
-app.post('/students', jsonParser, (request, response) => {
-    data.push(request.body);
-    fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
-    response.end();
-});
-
-
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
